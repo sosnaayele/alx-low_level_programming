49cd68e172f9d4c6db178c5eded06c270edd56bc
sosnaayele <sosna.ayele@gmail.com>
2023-03-08 10:49:08 -0800

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
-#inclunde "main.h"
+#include "main.h"
 
-/** 
- * _puts_recusion - prints a string
- * @s: string
- * *Return: none
+/**
+ * _puts_recursion - prints a string.
+ * @s: string.
+ * Return: None.
  */
+
 void _puts_recursion(char *s)
 {
+
 	if (*s == '\0')
 	{
-	_putchar('\n');
-	return;
+		_putchar('\n');
+		return;
 	}
+
 	_putchar(*s);
 	s++;
 	_puts_recursion(s);
