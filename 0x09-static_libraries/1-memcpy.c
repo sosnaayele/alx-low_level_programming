f2d1d3fa9ac11199ca3206c373a9d582cc94aaab
sosnaayele <sosna.ayele@gmail.com>
2023-03-12 01:34:46 -0800

    static library

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies meory area
+ * @dest: memory where is stored
+ * @scr: memory where is copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *scr, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = scr[r];
+		n--;
+	}
+	return (dest);
+}
