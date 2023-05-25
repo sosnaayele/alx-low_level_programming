f6d41dd0a3aa89ec149347c22618ef09b87cb5e1
sosnaayele <sosna.ayele@gmail.com>
2023-05-25 01:38:23 -0700

    Is digit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - check for upper case latter
  * @c: character to check
@@ -7,7 +8,7 @@
  */
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
