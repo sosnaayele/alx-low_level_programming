608f391defeaa228cb65d82e6c2e32ecf43fda6e
sosnaayele <sosna.ayele@gmail.com>
2023-05-30 23:51:48 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * print_times_table - prints the times table for n.
- * 
+ *
  * @n: The multiplication table requested.
- * 
  * Return: Nothing.
  */
 void print_times_table(int n)
